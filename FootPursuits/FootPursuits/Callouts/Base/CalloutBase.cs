521ec57972bd6a368af0ec58f7ac88678b58f811
Maurice Moss <cammygamesyt@gmail.com>
2016-07-03 15:23:40 +0100

    Bas.

---
+++ FootPursuits/FootPursuits/Callouts/Base/CalloutBase.cs
@@ -0,0 +1,10 @@
+﻿using LSPD_First_Response.Mod.API;
+using LSPD_First_Response.Mod.Callouts;
+
+namespace FootPursuits.Callouts.Base
+{
+    public class CalloutBase : Callout
+    {
+
+    }
+}
---
+++ FootPursuits/FootPursuits/Util/Common.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootPursuits.Util
+{
+    public class Common
+    {
+        public static Random random = new Random();
+    }
+}
