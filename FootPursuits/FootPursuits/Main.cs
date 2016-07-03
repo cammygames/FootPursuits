e3a39a123b8264699d39f65f924d0c7c44a34c2d
Maurice Moss <cammygamesyt@gmail.com>
2016-07-03 15:16:09 +0100

    Initial Commit.

---
+++ FootPursuits/FootPursuits/Main.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rage;
+using LSPD_First_Response.Mod.API;
+
+namespace FootPursuits
+{
+    public class Main : Plugin
+    {
+        public override void Initialize()
+        {
+            Functions.OnOnDutyStateChanged += OnDutyStateChangedEventHandler;
+            Game.LogTrivial("Plguin FootPursuits " + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString() + " by Maurice Moss & Sam Collins has been initialised.");
+        }
+        public override void Finally()
+        {
+            Game.LogTrivial("Plguin FootPursuits " + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString() + " plugin cleaned up.");
+        }
+
+        private static void OnDutyStateChangedEventHandler(bool OnDuty)
+        {
+            if (OnDuty)
+            {
+                RegisterCallouts();
+            }
+        }
+
+        private static void RegisterCallouts()
+        {
+            //Functions.RegisterCallout(typeof(Callouts.RTC));
+            Game.DisplayNotification("~o~Foot Pursuits ~g~V" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString() + " ~w~by ~b~Maurice Moss & Sam Collins ~w~has been initialised.");
+        }
+    }
+}
